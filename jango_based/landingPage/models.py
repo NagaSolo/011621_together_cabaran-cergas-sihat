ec9b6c989ecaedd0184ea8edb8851a5f89bb3fcc
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-02-02 14:50:38 +0000

    added; user models

--- jango_based/landingPage/models.py
+++ jango_based/landingPage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Profile(models.Model):
+    mobile_no = models.CharField(max_length=20)
+    pro_pic = models.ImageField(upload_to='users/pro_pics', blank=True, null=True)
+    portfolio_url = models.URLField(blank=True, null=True)
+
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+
+    status = models.CharField(max_length=10, blank=True, null=True, default='False')
+
+    def __str__(self):
+        return self.user.username

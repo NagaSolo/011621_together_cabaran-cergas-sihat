5a2de834081d3a41d6bcde0dfba1a6ded13f10cb
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-02-05 09:14:48 +0000

    updated; landing pages

--- jango_based/keluargaku/settings.py
+++ jango_based/keluargaku/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'landingPage.apps.LandingpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landingPage',
 ]
 
 MIDDLEWARE = [
--- jango_based/keluargaku/urls.py
+++ jango_based/keluargaku/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rumah/', include('landingPage.urls')),
+    path('landing/', include('landingPage.urls')),
 ]

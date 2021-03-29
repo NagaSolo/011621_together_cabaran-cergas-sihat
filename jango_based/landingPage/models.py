287dc2dc941cd21a5daf733383c977a16bf44328
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-03-29 15:38:40 +0100

    modified; views

--- jango_based/landingPage/models.py
+++ jango_based/landingPage/models.py
@@ -3,18 +3,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# # Create your models here.
-# class Profile(models.Model):
-#     mobile_no = models.CharField(max_length=20)
-#     pro_pic = models.ImageField(upload_to='users/pro_pics', blank=True, null=True)
-
-#     user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
-
-#     status = models.CharField(max_length=10, blank=True, null=True, default='False')
-
-#     def __str__(self):
-#         return self.user.username
-
 class Post(models.Model):
     
     title = models.CharField(max_length=100)
--- jango_based/landingPage/views.py
+++ jango_based/landingPage/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+from .models import Post
 def rumah(request):
-    return render(request, 'landing/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'landing/home.html', context)
 
 def tentang(request):
     return render(request, 'landing/about.html', {'title': 'About'})

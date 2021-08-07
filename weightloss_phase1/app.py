d161b30d8426019c46b89a38b26b3149624ad1e1
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-08-07 06:44:39 +0100

    added; scripts to generate graph

---
+++ weightloss_phase1/app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+df_progress = pd.read_csv(
+    'datasets/progress_phase1.csv', 
+    sep=',', 
+    thousands=' ')
+
+df_progress[['Minggu', 'Abe Kamil', 'Abe Isey', 'Kak Dayah', 'Kak Teh', 'Adz', 'Auni', 'Anis']].apply(pd.to_numeric, errors='coerce')
+
+""" Graf bagi setiap peserta """
+def output_graph(name):
+    df_progress.plot(x ='Minggu', y=name, kind = 'line')
+    plt.savefig('output/' + name + '.png')
+    return f'Graph for {name} done'
+
+print(df_progress.columns)
+
+for person in df_progress.columns[1:]:
+    output_graph(person)

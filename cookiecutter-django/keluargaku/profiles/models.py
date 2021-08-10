73bf26b7bed1c1c6da7bfc1728d7fc7ab6638ed3
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-08-10 10:10:55 +0100

    added; forms, urls, models, views, unofficial

--- cookiecutter-django/keluargaku/profiles/models.py
+++ cookiecutter-django/keluargaku/profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+from PIL import Image
+from django.db.models.fields.related import OneToOneField
+
+class ProfileImage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    picture = models.ImageField(default='default.jpg', upload_to='images/profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'

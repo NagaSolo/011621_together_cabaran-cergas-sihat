7608f5cef72f5509cea960149741033870ba0098
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-08-21 16:26:22 +0100

    added; tkinter based window

--- weightloss_phase1/app.py
+++ weightloss_phase1/app.py
@@ -2,6 +2,20 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
+import tkinter as tk
+
+class Window:
+    def __init__(self, root, title, geometry, message):
+        self.root = root
+        self.root.title(title)
+        self.root.geometry(geometry)
+        tk.Label(self.root, message).pack()
+
+def main():
+    root = tk()
+    windows1 = Window(root, 'Example', '300x300', 'Window 1 From class Window')
+
+
 df_progress = pd.read_csv(
     'datasets/progress_phase1.csv', 
     sep=',', 

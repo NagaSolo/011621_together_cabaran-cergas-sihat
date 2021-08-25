1b64b76e4f236f812e28102e4e09e0b4830231f7
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-08-25 14:27:14 +0100

    added; class to plot all data

--- weightloss_phase1/app.py
+++ weightloss_phase1/app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 class IndividualProgressChart:
     def __init__(self, name : str):
         self.name = name
-        self.df = pd.read_csv('datasets/progress_phase1.csv', sep=',', thousands=' ')
+        self.df = pd.read_csv('datasets/progress_phase1_no_NaN.csv', sep=',', thousands=' ')
 
     # df_progress[['Minggu', 'Abe Kamil', 'Abe Isey', 'Kak Dayah', 'Kak Teh', 'Adz', 'Auni', 'Anis']].apply(pd.to_numeric, errors='coerce')
 
@@ -21,8 +21,32 @@ class IndividualProgressChart:
         else:
             return f'No {self.name}'
 
+class AllProgressChart:
+    def __init__(self):
+        self.df = pd.read_csv('datasets/progress_phase1_no_NaN.csv', sep=',', thousands=' ')
+
+    """ Graf bagi setiap peserta """
+    def output_together_graph(self):
+        # self.df.set_index('Minggu')
+        new_df = pd.DataFrame(
+            data=self.df, 
+            columns=['Minggu', 'Abe Kamil', 'Abe Isey', 'Kak Dayah', 'Kak Teh', 'Adz', 'Auni', 'Anis'])
+        
+        new_df.plot(
+            x ='Minggu', 
+            y=['Abe Kamil', 'Abe Isey', 'Kak Dayah', 'Kak Teh', 'Adz', 'Auni', 'Anis'], 
+            kind = 'line',
+            figsize = (10, 15),
+            grid = True)
+        
+        plt.savefig('output/semua.png')
+        
+        return f'Graph for semua done'
+
 if __name__ == '__main__':
     # print(df_progress.columns)
 
     for person in ['Abe Kamil', 'Abe Isey', 'Kak Dayah', 'Kak Teh', 'Adz', 'Auni', 'Anis']:
         print(IndividualProgressChart(person).output_graph())
+
+    print(AllProgressChart().output_together_graph())

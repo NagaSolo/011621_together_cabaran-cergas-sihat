d7ef161dd3f2e32c11ee3d0135a8fee93fd6082a
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-09-04 15:32:53 +0100

    using; dataclass to group data into participant class

--- weightloss_phase1/app_dataclass.py
+++ weightloss_phase1/app_dataclass.py
@@ -4,17 +4,19 @@ from typing import List
 import pandas as pd
 
 @dataclass
-class Minggu:
-
-    minggu : str
-    berat : List
-
-@dataclass
 class Peserta:
     
     name : str
-    progress : List[Minggu]
+    progress : List
 
 if __name__ == '__main__':
+    pserta_peserta = []
+
     df = pd.read_csv('datasets/progress_phase1_no_NaN.csv', sep=',', thousands=' ')
-    print(df.columns)
+    
+    # print(df['Adz'].values.tolist())
+    for n in df.columns[1:]:
+        peserta_dan_progress = df[n]
+        pserta_peserta.append(Peserta(n, peserta_dan_progress.values.tolist()))
+
+    print(pserta_peserta)

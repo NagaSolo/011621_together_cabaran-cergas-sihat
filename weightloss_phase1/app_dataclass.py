d0fc158451cbc3e9242168cea2180f4bb42e3b62
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-09-05 15:16:10 +0100

    fixed; typo

--- weightloss_phase1/app_dataclass.py
+++ weightloss_phase1/app_dataclass.py
@@ -10,13 +10,13 @@ class Peserta:
     progress : List
 
 if __name__ == '__main__':
-    pserta_peserta = []
+    peserta_peserta = []
 
     df = pd.read_csv('datasets/progress_phase1_no_NaN.csv', sep=',', thousands=' ')
     
     # print(df['Adz'].values.tolist())
     for n in df.columns[1:]:
         peserta_dan_progress = df[n]
-        pserta_peserta.append(Peserta(n, peserta_dan_progress.values.tolist()))
+        peserta_peserta.append(Peserta(n, peserta_dan_progress.values.tolist()))
 
-    print(pserta_peserta)
+    print(peserta_peserta)
